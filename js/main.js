7f8fe02fbdff3a17df60033f335371069d2bc73c
rohan kumar <rohanvkumarv@gmail.com>
2026-03-06 19:58:17 +0530

    done

---
+++ js/main.js
@@ -0,0 +1,51 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.navbar').addClass('sticky-top shadow-sm');
+        } else {
+            $('.navbar').removeClass('sticky-top shadow-sm');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    // Dynamically load header and footer
+    $(function () {
+        $("#header-placeholder").load("includes/header.html");
+        $("#footer-placeholder").load("includes/footer.html", function() {
+            // Re-initiate WOW.js on dynamically loaded footer elements
+            new WOW().init();
+        });
+    });
+    
+})(jQuery);
